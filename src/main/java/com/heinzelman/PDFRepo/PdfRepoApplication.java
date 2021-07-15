a0d0b9c06e9cf8807a2a862fbed8fac540c29ad2
Piotr Heinzelman <piotr@i-zn.pl>
2021-07-15 19:52:30 +0200

    Spring initializr

---
+++ src/main/java/com/heinzelman/PDFRepo/PdfRepoApplication.java
@@ -0,0 +1,13 @@
+package com.heinzelman.PDFRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heinzelman/PDFRepo/PdfRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heinzelman.PDFRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
